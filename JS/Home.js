cc8d2174e122bb083e077a8f9d8945a8e8e4fa6c
sai-teja-climentos <saiteja.b@climentos.com>
2026-06-02 12:06:47 +0530

    6/2/2026
    6/2/2026 ++ day-1

---
+++ JS/Home.js
@@ -0,0 +1,64 @@
+// a = 10
+
+// b = 20
+// c = b-a
+
+// console.log(b-a)
+
+// a = 'HI'
+// b = 'Ram'
+
+// c = "By Useing ,"
+// d = "By Useing +"
+
+// console.log(c+a,b)
+// console.log(d+a+b) //By Useing + 1020
+
+
+// console.log(c,a+b) //By Useing , 30
+
+// console.log(a+b+d) //30By Useing + 
+
+// a = 13
+
+// b = 13
+
+// c = a<b 
+
+// console.log(c) //false
+
+
+// a = 13
+
+// b = 13
+
+// c = a>b 
+
+// console.log(c) //false
+
+
+// a = 13
+
+// b = 13
+
+// c = a<=b 
+
+// console.log(c) //true
+
+
+// a = 13
+
+// b = 13
+
+// c = a>=b 
+
+// console.log(c) //true
+
+// a = 13
+
+// b = 13
+
+// c = a==b 
+
+// console.log(c) //true
+
