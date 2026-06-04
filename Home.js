5a31ed54ecd93934e05a765350bb2aa7d794ddfc
sai-teja-climentos <saiteja.b@climentos.com>
2026-06-04 12:59:30 +0530

    6-4-2026
    6-4-2026+++

---
+++ Home.js
@@ -0,0 +1,75 @@
+// function test(){
+//   var age = true;
+
+//     console.log(age)
+// }
+// test();
+
+// var Keyword in JavaScript
+// The var keyword is used to declare a variable. 
+// It can be accessed throughout the function in 
+// which it is declared, or globally if declared 
+// outside a function.
+
+// var age = 23;
+
+
+// let Keyword in JavaScript
+// The let keyword is used to declare a block-scoped variable.
+// It was introduced in ES6 and is the preferred way to declare
+// variables that may change.
+
+
+// Declaring a Variable
+
+// let name = "Ram"
+
+// name = "sam"
+
+// console.log(name)
+
+// const a=10
+
+// a = 20 
+
+// console.log(a) //Erorr
+
+// Syntax
+// var variableName = value;
+// let variableName = value;
+// const variableName = value;
+
+// var name 
+
+// console.log(name) //undefined
+
+// name = "Ram"
+
+// Variable Names Can Contain
+// Letters (a-z, A-Z)
+// Numbers (0-9)
+// Underscore (_)
+// Dollar sign ($)
+// a=10
+// b=10
+// c="TEN"
+
+// d =c+b+a
+// console.log(d)
+
+// | Variable Name | Valid? | Reason               |
+// | ------------- | ------ | -------------------- |
+// | `name`        | ✅      | Valid                |
+// | `userName`    | ✅      | Valid                |
+// | `_count`      | ✅      | Valid                |
+// | `$price`      | ✅      | Valid                |
+// | `name1`       | ✅      | Valid                |
+// | `1name`       | ❌      | Starts with a number |
+// | `first name`  | ❌      | Contains a space     |
+// | `user-name`   | ❌      | Contains `-`         |
+// | `function`    | ❌      | Reserved keyword     |
+
+
+
+// let a = "ZYX";
+ 
