7a46af965aacb9f8a6596e775a237c5c7c7fa371
sai-teja-climentos <saiteja.b@climentos.com>
2026-06-10 12:04:34 +0530

    Loops++
    6/10/2026

---
+++ Loops.js
@@ -0,0 +1,28 @@
+// i = 1;
+
+// j = 10;
+// for(i;i<=j;i++){
+//     console.log(i)
+// }
+
+
+// let i = 0;
+// while (i < 10){
+//     console.log(i)
+//     i++
+// }
+
+
+
+
+// let i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+
+// } while(i<5);
+
+
+
+
